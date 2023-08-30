ba9ff5cc8c3b5b44ec01044fe76404c024ebd611
Cortland Diehm <cortlanddiehm@gmail.com>
2023-08-30 00:50:08 -0700

    Best Time to Buy and Sell Stock

---
+++ BestWorstTimeStock.py
@@ -0,0 +1,27 @@
+#Leetcode Problem: Best Time to Buy and Sell Stock
+
+#Two pointers, left pointer behind right
+
+#Variable for current max, "maxP"
+
+#While right pointer is less than the length of prices,
+#if left is less than right then the profit is right - left
+#maxP = max(maxP, profit) <-- maxP is the maximum number between maxP and profit
+
+#increment right by 1 
+
+class Solution(object):
+    def maxProft(self, prices: List[int]) -> int:
+        left, right = 0, 1
+        maxP = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - profit[left]
+                maxP = max(maxP, profit)
+            else:
+                left = right
+            
+            right += 1
+
+        return maxP
